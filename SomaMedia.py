ca75d734f140db47d82cfd530f6e694e57be8f1d
CrysKawaii <cryskawaiii@gmail.com>
2023-03-08 16:19:22 -0300

    Mostra a soma e a média de 5 números

---
+++ SomaMedia.py
@@ -0,0 +1,7 @@
+addiction = 0
+for c in range(5):
+    num = int(input('Digite um número: '))
+    addiction += num
+average = addiction / 5
+print(f'A soma dos números é {addiction}')
+print(f'A média deles é {average}')
