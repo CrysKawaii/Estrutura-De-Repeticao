57a2315b33b4e0457cdbfdc56f16e3b045cca967
CrysKawaii <cryskawaiii@gmail.com>
2023-03-12 12:04:16 -0300

    Agora também mostra as médias de altura e peso

--- Academia.py
+++ Academia.py
@@ -1,4 +1,4 @@
-higher_height = smaller_height = average = count = fat = lean = 0
+higher_height = smaller_height = average_height = average_weight = count = fat = lean = 0
 code_higher = code_smaller = code_fat = code_lean = 0
 while True:
     cod = int(input('Digite seu código: '))
@@ -27,7 +27,11 @@ while True:
             lean = weight
             code_lean = cod
     count += 1
+    average_height += height / count
+    average_weight += weight / count
 print(f'O cliente mais alto é o do código {code_higher} com {higher_height} de altura')
 print(f'O cliente mais baixo é o do código {code_smaller} com {smaller_height} de altura')
 print(f'O mais gordo é o do código {code_fat} pesando {fat}')
 print(f'O mais magro é o do código {code_lean} pesando {lean}')
+print(f'A média entre as alturas é {average_height:.2f}')
+print(f'A média entre os pesos é {average_weight:.1f}')
